985a0c0501f3675eebda5f060b90387de1525698
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-05-31 21:53:38 +0530

    Add the basic component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Outlet, useNavigate } from 'react-router-dom'
+import authService from './Appwrite/auth'
+import { logIn, logOut } from './store/authSlice'
+import { Footer, Header } from './component'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(logIn({ userData }))
+        } else {
+          dispatch(logOut())
+        }
+      })
+      // .catch()
+      .finally(() => { setLoading(false) })
 
-  return (
-    <div>
-      <p>Blog app with appwrite</p>
+  }, []);
+  return !loading ? (
+    <div className=' min-h-screen flex flex-wrap justify-between bg-gray-400'>
+      <div className=' w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
     </div>
-  )
+  ) : null
 }
 
 export default App
