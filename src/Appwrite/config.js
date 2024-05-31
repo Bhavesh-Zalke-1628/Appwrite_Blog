70e21d21c4f01e2c1635c33906f2c7bd1205c7ab
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-05-31 21:21:17 +0530

    Add file config file

---
+++ src/Appwrite/config.js
@@ -0,0 +1,142 @@
+import { connect } from "react-redux";
+import conf from "../conf/conf.js";
+import { Client, Databases, ID, Storage, Query } from "appwrite";
+import { configureStore } from "@reduxjs/toolkit";
+
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWriteURl)
+            .setProject(conf.appWriteProjectID)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+
+            return true;
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    async getPosts(quires = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                quires
+            )
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    // file upload service
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    // delete file
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appWriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    // file priview
+
+    getFilePriview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.appWriteBucketId,
+            fileId
+        )
+    }
+}
+
+
+const service = new Service()
+
+
+export default service;
